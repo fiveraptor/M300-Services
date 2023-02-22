470c7ad78f5141e761fef696bd795fe3fad003e7
fiveraptor <jorisbieg@gmail.com>
2023-02-22 17:27:11 +0100

    AWS changes

--- AWS/Ubuntu2204Server/config.rb
+++ AWS/Ubuntu2204Server/config.rb
@@ -7,7 +7,7 @@ $aws_options[:ec2_keypair] = "vagrant.pem"
 # Region Frankfurt 
 $aws_options[:region] = "eu-central-1"
 # Ubuntu 14.04 Images 
-$aws_options[:ami_id] = "ami-26c43149"
+$aws_options[:ami_id] = "ami-0d1ddd83282187d18"
 $aws_options[:instance_type] = "t2.micro"
 # Der Name der erstellten Security Group
 $aws_options[:security_group] = "vagrant"
