c9320a61de62647d79f3c8ba2f0e211cd69a059c
fiveraptor <jorisbieg@gmail.com>
2023-02-27 15:05:51 +0100

    AWS updated

--- AWS/Ubuntu2204Server/config.rb
+++ AWS/Ubuntu2204Server/config.rb
@@ -1,13 +1,11 @@
 $aws_options = {}
 # Access und Secret Key vom User vagrant-user
-$aws_options[:access_key] = ""
-$aws_options[:secret_key] = ""
+
 # Der Name des erstellten Key Pairs
-$aws_options[:ec2_keypair] = "vagrant.pem"
 # Region Frankfurt 
 $aws_options[:region] = "eu-central-1"
 # Ubuntu 14.04 Images 
-$aws_options[:ami_id] = "ami-0d1ddd83282187d18"
+$aws_options[:ami_id] = "ami-0e067cc8a2b58de59"
 $aws_options[:instance_type] = "t2.micro"
 # Der Name der erstellten Security Group
 $aws_options[:security_group] = "vagrant"
